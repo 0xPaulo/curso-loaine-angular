48498960e33bfc5d62af40a1ddd6506a74777eb9
0xPaulo <paulo_czar@outlook.com.br>
2023-11-08 17:33:11 -0300

    primeira-rota :+1:

--- projects/2-rotas/src/app/app.module.ts
+++ projects/2-rotas/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import 'materialize-css';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app.routing';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, LoginComponent, CursosComponent],
+  imports: [BrowserModule, AppRoutingModule, routing],
   providers: [],
   bootstrap: [AppComponent],
 })
