f6908c8aa9b2a7f54025ef7f942286e4bffdaa8f
0xPaulo <paulo_czar@outlook.com.br>
2023-11-10 14:05:08 -0300

    extraindo-subscribe :+1:

--- projects/2-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ projects/2-rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.scss'],
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
   id: string = '';
+  inscricao: Subscription = new Subscription();
 
   constructor(private _route: ActivatedRoute) {
-    this.id = _route.snapshot.params['id'];
-    console.log(this._route);
+    // this.id = _route.snapshot.params['id'];
+    // console.log(this._route);
+    // Pega so uma vez
+  }
+  ngOnInit() {
+    this.inscricao = this._route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 }
