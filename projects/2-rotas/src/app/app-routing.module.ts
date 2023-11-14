8bf7556e0f12fe446eba7213b3fd8bbc2014e261
0xPaulo <paulo_czar@outlook.com.br>
2023-11-14 18:51:31 -0300

    organizando-modulo-rotas

--- projects/2-rotas/src/app/app-routing.module.ts
+++ projects/2-rotas/src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'cursos', component: CursosComponent },
-  { path: 'curso/:id', component: CursoDetalheComponent },
-  { path: 'nao-encontrado', component: CursoNaoEncontradoComponent },
 ];
 
 @NgModule({
--- projects/2-rotas/src/app/app.module.ts
+++ projects/2-rotas/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosModule } from './cursos/cursos.module';
+import { CursoRoutingModule } from './cursos/curso-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, CursosModule],
+  imports: [BrowserModule, CursosModule, AppRoutingModule, CursoRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
